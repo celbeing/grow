840622bdbaf2f2efcd2dd1c5f909726312a28c3f
전라남도교육지원청 <kimsd1983@naver.com>
2025-05-25 17:58:08 +0900

    csv 파일 제작

--- django/grow_project/students_app/urls.py
+++ django/grow_project/students_app/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.verify_with_dropdown, name='verify_dropdown'),
+    path('<int:student_id>/avatar/', views.student_avatar, name='student_avatar'),
+]
+'''
+urlpatterns = [
     path('', views.student_list, name='student_list'),
     path('<int:student_id>/avatar/', views.student_avatar, name='student_avatar'),
 ]
+'''
--- django/grow_project/students_app/views.py
+++ django/grow_project/students_app/views.py
@@ -1,5 +1,25 @@
-﻿from django.shortcuts import render, get_object_or_404
+﻿from django.shortcuts import render,  redirect, get_object_or_404
 from .models import Student
+
+def verify_with_dropdown(request):
+    error = None
+    students = Student.objects.all()
+
+    if request.method == 'POST':
+        student_id = request.POST.get('student_id')
+        input_code = request.POST.get('code')
+
+        student = get_object_or_404(Student, id=student_id)
+
+        if input_code == student.code:
+            return redirect('student_avatar', student_id=student.id)
+        else:
+            error = "코드가 일치하지 않습니다."
+
+    return render(request, 'students_app/verify_dropdown.html', {
+        'students': students,
+        'error': error
+    })
 
 def student_list(request):
     students = Student.objects.all()
