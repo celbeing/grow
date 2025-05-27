864dfd2e9b7b7a52f3a980c7c8deb346d64ce390
전라남도교육지원청 <kimsd1983@naver.com>
2025-05-27 18:17:46 +0900

    과목 선택 화면 구현

--- django/grow_project/students_app/urls.py
+++ django/grow_project/students_app/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.avatar_view, name='avatar_view'),
-
+    path('<int:student_id>/select_subject/', views.select_subject, name='select_subject'),
+    path('<int:student_id>/<str:subject>/', views.show_avatar, name='show_avatar'),
 ]
-'''
-urlpatterns = [
-    path('', views.student_list, name='student_list'),
-    path('<int:student_id>/avatar/', views.student_avatar, name='student_avatar'),
-]
-'''
--- django/grow_project/students_app/views.py
+++ django/grow_project/students_app/views.py
@@ -1,10 +1,23 @@
 ﻿from django.shortcuts import render, redirect
 from .utils import load_students_from_csv, render_avatar_text, validate_student_code_by_name
 
+def select_subject(request, student_id):
+    # 코드 인증 성공 후 이 페이지로 redirect
+    return render(request, 'students_app/select_subject.html',{
+        'student_id': student_id
+    })
+
+def show_avatar(request, subject):
+    # 선택된 과목을 기반으로 아바타 출력
+    # 이 부분은 나중에 이미지로 대체할 예정
+    return render(request, 'students_app/avatar.html', {
+        'subject': subject
+    })
+
+
 def avatar_view(request):
     df = load_students_from_csv()
     names = df['name'].tolist()
-    avatar_output = ""
     selected_name = ""
     error = ""
 
@@ -15,12 +28,12 @@ def avatar_view(request):
         student_data, error = validate_student_code_by_name(df, selected_name, code_input)
 
         if not error:
-            avatar_output = render_avatar_text("all", student_data)
+            student_id = student_data['id']
+            return redirect('select_subject', student_id=student_id)
 
     return render(request, 'students_app/avatar_form.html', {
         'student_names': names,
         'selected_name': selected_name,
-        'avatar_output': avatar_output,
         'error': error
     })
 
